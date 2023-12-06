b2b9c4977b56a1dd94cd9dce2c7a7a322dd77751
El Hadji Abdoulaye Thiam <elhadjiabdoulaye.thiam@epsilon-france.com>
2023-12-06 13:01:07 +0100

    update r

--- main.py
+++ main.py
@@ -9,28 +9,24 @@ import os
 app = FastAPI()
 
 class InputText(BaseModel):
-    text: str
+    recordId: str
+    data: dict
 
 class InputData(BaseModel):
     values: List[InputText]
-
-class ResponseData(BaseModel):
-    recordId: int
-    data: dict
 
 model = SentenceTransformer(os.path.join(os.getcwd(), 'all-MiniLM-L6-v2'))
 
 @app.post("/custom_embedding")
 async def custom_embedding(input_data: InputData):
     logging.info('Python HTTP trigger function processed a request.')
-    logging.info(input_data)
     print(input_data)
-    input_text = [value.text for value in input_data.values]
+    input_text = [value.data['text'] for value in input_data.values]
 
     if not input_text:
         return {"error": "No input text found"}
 
     embeddings = model.encode(input_text)
-    response = { "values": [ { "recordId": i, "data": { "vector": embedding.tolist() } } for i, embedding in enumerate(embeddings)]}
+    response = { "values": [ { "recordId": int(value.recordId), "data": { "vector": embedding.tolist() } } for value, embedding in zip(input_data.values, embeddings)]}
 
     return response
