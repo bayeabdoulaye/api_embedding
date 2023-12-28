3b383d55ec22aafe20ebcea26cddc14bf0f69313
El Hadji Abdoulaye Thiam <elhadjiabdoulaye.thiam@epsilon-france.com>
2023-12-28 15:53:46 +0100

    update input split text endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from typing import List, Optional
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -21,12 +22,34 @@ class TextSplitInput(BaseModel):
     text: str
     mode: str
 
+class VectorData(BaseModel):
+    vector: List[float]
+class ResponseItem(BaseModel):
+    recordId: int
+    data: VectorData
+
+class ResponseData(BaseModel):
+    values: List[ResponseItem]
+
 class ChunkOutput(BaseModel):
     recordId: str
     data: dict
+class TextSplitInput(BaseModel):
+    values: List[dict]
+
+class TextSplitOutput(BaseModel):
+    recordId: str
+    data: dict
+
+class TextItemOutput(BaseModel):
+    textItems: List[str]
+
+class TextSplitResponse(BaseModel):
+    values: List[TextSplitOutput]
 
 model = SentenceTransformer(os.path.join(os.getcwd(), 'all-MiniLM-L6-v2'))
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1024,chunk_overlap = 20)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1024,chunk_overlap = 40)
+
 @app.post("/custom_embedding")
 async def custom_embedding(input_data: InputData):
     logging.info('Python HTTP trigger function processed a request.')
@@ -42,9 +65,32 @@ async def custom_embedding(input_data: InputData):
     return response
 
 @app.post("/text_split")
-async def text_split(input_data: TextSplitInput):
-    if input_data.mode == "sentence":
-        doc_text = text_splitter.create_documents([input_data.text])
-        chunks = [doc.dict()["page_content"] for doc in doc_text]
-        response = { "recordId": input_data.recordId, "data": { "chunks": chunks } }
-        return response
+async def text_split(text_split: TextSplitInput):
+    try:
+        response_values = []
+        for item in text_split.values:
+            record_id = item["recordId"]
+            text = item["data"]["text"]
+
+            doc_chunks = text_splitter.create_documents([text])
+            chunks = [doc.dict()["page_content"] for doc in doc_chunks]
+            text_items = []
+            for chunk in chunks:
+                text_items.append(chunk)
+
+            response_values.append({
+                        "recordId": record_id,
+                        "data": {"textItems": text_items}
+                    })
+
+        response = {"values": response_values}
+        return JSONResponse(status_code=200, content=response)
+    except Exception as e:
+        error_message = f'Exception => {e}'
+        print(error_message)
+        return JSONResponse(
+                status_code=400,
+                content=error_message
+            )
+
+        
